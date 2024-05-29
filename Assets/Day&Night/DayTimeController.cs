5f857a626f01018c438b5502995a35d6d8fc136e
Ksumba2003 <senysanydrag@mail.ru>
2024-05-29 17:59:24 +0300

    Light

--- Assets/Day&Night/DayTimeController.cs
+++ Assets/Day&Night/DayTimeController.cs
@@ -1,18 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class DayTimeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    const float secondsInDay = 86400f;
+
+    [SerializeField] Color nightLightColor;
+    [SerializeField] AnimationCurve nightTimeCurve;
+    [SerializeField] Color dayLightColor = Color.white;
+
+    float time;
+    [SerializeField] float timeScale = 60f;
+
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] Light2D globalLight;
+    private int days;
+
+    float Hours
     {
-        
+        get
+        {
+            return time / 3600f;
+        }
+    }
+    float Minutes
+    {
+        get
+        {
+            return time % 3600f / 60f;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        time += Time.deltaTime * timeScale;
+        int hh = (int)Hours;
+        int mm = (int)Minutes;
+        text.text = hh.ToString("00") + ":" + mm.ToString("00"); ;
+        float v = nightTimeCurve.Evaluate(Hours);
+        Color c = Color.Lerp(dayLightColor, nightLightColor, v);
+        globalLight.color = c;
+        if (time > secondsInDay)
+        {
+            NextDay();
+        }
+    }
+
+    private void NextDay()
+    {
+        time = 0;
+        days += 1;
     }
 }
