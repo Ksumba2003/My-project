bcd4ce97cf9b7113ea863f877b070bf3bb0b0bb0
Ksumba2003 <senysanydrag@mail.ru>
2024-05-30 11:49:43 +0300

    Transition

--- Assets/Scriptes/Transition.cs
+++ Assets/Scriptes/Transition.cs
@@ -2,15 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum TransitionType
 {
-    Warp, 
+    Warp,
     Scene
 }
 
 public class Transition : MonoBehaviour
 {
+    [SerializeField] TransitionType transitionType;
+    [SerializeField] string sceneNameToTransition;
+    [SerializeField] Vector3 targetPosition;
+
     Transform destination;
 
     // Start is called before the first frame update
@@ -21,7 +26,29 @@ public class Transition : MonoBehaviour
 
     internal void InitiateTransition(Transform toTransition)
     {
-        toTransition.position = destination.position;
-    }
+        Cinemachine.CinemachineBrain currentCamera = Camera.main.GetComponent<Cinemachine.CinemachineBrain>();
+        
+        switch (transitionType)
+        {
+            case TransitionType.Warp:
+                currentCamera.ActiveVirtualCamera.OnTargetObjectWarped(
+                    toTransition,
+                    destination.position - toTransition.position
+                    );
+                toTransition.position = new Vector3(
+                    destination.position.x,
+                    destination.position.y,
+                    toTransition.position.z
+                );
+                break;
+            case TransitionType.Scene:
+                currentCamera.ActiveVirtualCamera.OnTargetObjectWarped(
+                    toTransition,
+                    targetPosition - toTransition.position
+                    );
 
+                GameSceneManager.instance.SwitchScene(sceneNameToTransition, targetPosition);
+                break;
+        }
+    }
 }
